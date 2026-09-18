35581225bdddebbb35cef2f2fb59d145c2fc6d5a
Crystian Jesus <crystianjs09@gmail.com>
2026-09-18 02:17:54 +0000

    ajuste

--- .next/dev/server/server-reference-manifest.js
+++ .next/dev/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"lu1ZwHKZBVA9dWXqVED40ZcF4YcYIJncxUbposeSBNU=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"uaV78OpdCX7bQfQsNUhpz0p49CekgJc/Q0OVRF1yJrM=\"\n}"
